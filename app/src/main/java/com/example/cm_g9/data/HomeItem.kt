04ecf73e355ffff134e5b88e8554b07ffd120149
TheVaultBoy16 <gonaguiman@gmail.com>
2026-03-12 11:14:18 +0100

    Room inicial (no está bien importado creo)

--- app/src/main/java/com/example/cm_g9/data/HomeItem.kt
+++ app/src/main/java/com/example/cm_g9/data/HomeItem.kt
@@ -1,16 +1,16 @@
 package com.example.cm_g9.data
 
-import android.graphics.drawable.Drawable
-import androidx.core.graphics.drawable.toDrawable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "home_items")
 data class HomeItem(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val name: String,
     val imageRes: Int,
     val date: String = "22/02/2026",
     val horaUsadas: Long = 2,
     val minUsadas: Long = 3,
     val segUsadas: Long = 45,
-    val icono: Drawable = 0xFF0000FF.toInt().toDrawable(),
-    val habilitado: Boolean = true
 )
